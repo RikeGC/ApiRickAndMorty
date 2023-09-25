f06a62037a231ca95423a8943d193df2826f73e9
RikeGC <rikelmecarneiro@gmail.com>
2023-09-25 13:17:27 -0300

    Atualização de estilo e criação da page episodios

---
+++ JS/episodios.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    urlBase = 'https://rickandmortyapi.com/api/episode/?page=';
+    ListarTodos();
+    $("#btnNext").click(function () {
+        urlBase = page.next;
+        ListarTodos();
+        window.scrollTo(0,0);
+    });
+    
+    $("#btnPrev").click(function () {
+        urlBase = page.prev;
+        ListarTodos();
+        window.scrollTo(0,0);
+    });
+});
+
+function ListarTodos() {
+    $("#episodios > ul").empty();
+    $.ajax({
+        url: urlBase,
+        success: function (dados) {
+            dados.results.forEach(function (item) {
+                AdicionarLinha(item);
+                page = new Object(dados.info);
+                if (page.prev == null) {
+                    $("#btnPrev").attr("disabled","disabled");
+                }else{
+                    $("#btnPrev").removeAttr("disabled","disabled");
+                };
+                if (page.next == null) {
+                    $("#btnNext").attr("disabled","disabled");
+                }else{
+                    $("#btnNext").removeAttr("disabled","disabled");
+                };
+            })},
+        error: function (erro) {
+            alert({ html: 'Ocorreu algum Erro.\nTente novamente mais tarde.\n'+erro });
+        }
+    })
+}
+
+function AdicionarLinha(item) {
+    var novaLinha = $("<li>");
+    var col = '';
+    col += '<a href="#">' + item.name +' ['+ item.episode+ ']</a>';
+    novaLinha.append(col);
+    $("#episodios > ul").append(novaLinha);
+}
+
+function NewPage(id){
+    teste = Cookies.set('teste', id, {expires: 0.01});
+    alert('Funcionou ' + teste);
+}
