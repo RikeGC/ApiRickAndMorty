a4696c8275d54d3557f0c9ef3c32a4cac493314f
RikeGC <rikelmecarneiro@gmail.com>
2023-09-27 12:53:21 -0300

    Construido consumidor de episodio

--- JS/personagem.js
+++ JS/personagem.js
@@ -16,11 +16,30 @@ function PersonagemSelecionado(id) {
                 $(".reciveImage").attr("src",dados.image);
                 $(".reciveLocal").html(dados.location.name);
                 $(".reciveOrigem").html(dados.origin.name);
+                dados.episode.forEach(function (item) {
+                    $.ajax({
+                        url: item,
+                        success: function (dadosEpi) { 
+                            AdicionarLinha(dadosEpi);
+                        },
+                        error: function (erroEpi) {
+                            alert({ html: 'Ocorreu algum Erro.\nTente novamente mais tarde.\n'+erroEpi });
+                        }
+                    })                  
+                });
             },
         error: function (erro) {
             alert({ html: 'Ocorreu algum Erro.\nTente novamente mais tarde.\n'+erro });
         }
     })
+}
+
+function AdicionarLinha(item) {
+    var novaLinha = $("<h3>");
+    var col = '';
+    col += '['+item.episode+'] - '+ item.name;
+    novaLinha.append(col);
+    $(".episodios").append(novaLinha);
 }
 
 $( window ).on( "unload", function() {
